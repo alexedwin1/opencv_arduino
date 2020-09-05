bd5f8ac199c587e7d3200ea340143386c614503a
Edwin Alejandro Anona <alexedwin1@gmail.com>
2020-09-04 23:05:55 -0600

    Added Arduino code
    Pruebas

---
+++ OpenCV_arduino_LED/OpenCV_arduino_LED.ino
@@ -0,0 +1,42 @@
+/*
+  Programa para Arduino: este programa enciende el LED 13 cuando
+  se recibe el carácter 'h' por serial, y lo apaga si recibe otro.
+ 
+  Escrito por Pablo Becerra y Glare.
+*/
+ 
+//Se define el pin del LED:
+#define LED 13
+ 
+//Variable para almacenar el caracter que llega por serial:
+char lectura;
+ 
+void setup()
+{
+   //Se inicia el Serial:
+   Serial.begin(9600);
+ 
+   //Definir el pin 13 como output:
+   pinMode(LED, OUTPUT);
+}
+ 
+ 
+void loop()
+{
+   if(Serial.available() >= 1)
+   {
+      //Leemos el serial y guardamos el mensaje:
+      lectura = Serial.read();
+ 
+      //Si recibimos 'h', encendemos el LED:
+      if(lectura == 'h')
+      {
+         digitalWrite(LED, HIGH);
+      }
+      //En caso contrario, lo apagamos:
+      else
+      {
+         digitalWrite(LED, LOW);
+      }
+   }
+}
